747a6cc0895dd6c04b8c62aa2fdd2cdca8fe0c8f
chengjie <chengjie@unity-drive.com>
2021-06-10 11:26:00 +0800

    add launch env path

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ cd ${ROOT_DIR}/planning_core/3d_model && pwd && tar -xvf model3.tar.xz
 
 # build gtsam
 cd ${ROOT_DIR}/gp_planner/thirdparty/gtsam-4.1rc
-mkdir build install & cd build
+mkdir build install
+cd build
 cmake .. -DCMAKE_INSTALL_PREFIX="../install"
-make -j6 & make install
+make -j6 && make install
